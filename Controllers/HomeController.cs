de39e8f8d56a6745a3b959f2c4d123b1f2fc1090
Anders Sundin <olsu2201@student.miun.se>
2024-01-25 12:38:40 +0100

    kod för att rätta provet och tilldela poängen

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 
 
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using provet.Models;
@@ -40,7 +41,7 @@ namespace provet.Controllers
         {
             ViewData["title"] = "Startsidan"; // Sidans titel
 
-               // Spara sessions-variabler från formulär
+            // Spara sessions-variabler från formulär
             if (participant.Name != null && participant.HigherEd != null)
             {
                 HttpContext.Session.SetString("_name", participant.Name);
@@ -62,14 +63,50 @@ namespace provet.Controllers
         public IActionResult Questions()
         {
 
-         
+
             ViewBag.user = HttpContext.Session.GetString("_name");
-            
+
 
             ViewData["title"] = "Frågor"; // Sidans titel
             return View();
         }
 
+
+        // Hantera frågeformuläret
+
+        [HttpPost]
+        [Route("/fragor")] // ändrar routingen
+        public IActionResult Questions(AnswersModel answers)
+        {
+
+            // Skapa en varibel som håller antalet rätta svar
+            int points = 0;
+
+            // Rätta provet
+
+            if (answers.Question1 == "alternative3")
+            {
+                points++;
+            }
+            if (answers.Question2 == "alternative2")
+            {
+                points++;
+            }
+            if (answers.Question3 == "alternative2")
+            {
+                points++;
+            }
+            if (answers.Question4 == "alternative2")
+            {
+                points++;
+            }
+            if (answers.Question5 == "alternative1")
+            {
+                points++;
+            }
+            ViewBag.answer = points;
+            return View();
+        }
         /* RESULTAT */
         //-------------------------------------------------------//
 
